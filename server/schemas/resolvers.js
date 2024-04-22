938d950c462aa902d52fe5661aaee7c264686c78
Brock Lockhart-Doyle <blockdoyle@gmail.com>
2024-04-22 01:31:31 -0400

    Refactor server/schemas/resolvers.js to use AuthenticationError correctly

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,23 +3,17 @@ const { signToken, AuthenticationError } = require("../utils/auth");
 
 const resolvers = {
   Query: {
-    users: async () => {
-      return User.find().populate("savedBooks");
-    },
-    user: async (parent, { username }) => {
-      return User.findOne({ username }).populate("savedBooks");
-    },
     me: async (parent, args, context) => {
       if (context.user) {
         return User.findOne({ _id: context.user._id }).populate("savedBooks");
       }
-      throw new AuthenticationError("You need to be logged in!");
+      throw AuthenticationError;
     },
   },
 
   Mutation: {
-    addUser: async (parent, args) => {
-      const user = await User.create(args);
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
       const token = signToken(user);
 
       return { token, user };
@@ -68,3 +62,5 @@ const resolvers = {
     },
   },
 };
+
+module.exports = resolvers;
